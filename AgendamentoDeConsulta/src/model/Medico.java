9c6156a0bc2498a3d977f85e8645ba94fd630238
PauloCesar <andradeoliveira2004@icloud.com>
2025-05-22 14:16:17 -0300

    Teste

--- AgendamentoDeConsulta/src/model/Medico.java
+++ AgendamentoDeConsulta/src/model/Medico.java
@@ -1,4 +1,32 @@
 package model;
 
 public class Medico {
+
+    private String nomeMedico;
+    private String especialidade;
+    private int CRM;
+
+    public Medico(String nomeMedico, String especialidade, int CRM) {
+        this.nomeMedico = nomeMedico;
+        this.especialidade = especialidade;
+        this.CRM = CRM;
+    }
+
+    public String getNomeMedico() {
+        return nomeMedico;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public int getCRM() {
+        return CRM;
+    }
+
+    @Override
+    public String toString() {
+        return nomeMedico +
+                " especialidade: " + especialidade;
+    }
 }
--- AgendamentoDeConsulta/src/view/Main.java
+++ AgendamentoDeConsulta/src/view/Main.java
@@ -1,4 +1,73 @@
 package view;
 
+import model.Consulta;
+import model.Medico;
+import model.Paciente;
+import service.AgendamentoService;
+import service.MedicoRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        AgendamentoService consultasAgendadas = new AgendamentoService();
+
+        System.out.println("==== Agendamento de Consulta ====");
+
+        while (true) {
+
+            System.out.println("1 - Agendar consulta");
+            int opcoes = entrada.nextInt();
+
+            switch (opcoes) {
+                case 1:
+                    entrada.nextLine();
+                    System.out.println("Digite o seu nome: ");
+                    String nome = entrada.nextLine();
+
+                    System.out.println("Digite sua Idade: ");
+                    int idade = entrada.nextInt();
+
+                    System.out.println("Digite seu CPF: ");
+                    long CPF = entrada.nextLong();
+                    entrada.nextLine();
+
+                    System.out.println("Data (dd/mm/aaaa)");
+                    String data = entrada.nextLine();
+
+                    System.out.println("Horário (hh:mm): ");
+                    String horario = entrada.nextLine();
+
+                    List<Medico> medicosDisponiveis = MedicoRepository.getMedicosDisponiveis();
+                    System.out.println("Escolha o medico que você deseja passar: ");
+                    for (int i = 0; i < medicosDisponiveis.size(); i++) {
+                        Medico m = medicosDisponiveis.get(i);
+                        System.out.println((i + 1) + m.getNomeMedico() + " - " + m.getEspecialidade() + " - " + m.getCRM());
+                    }
+
+                    System.out.println("Escolha um através do número qual médico você deseja: ");
+                    int escolha = entrada.nextInt();
+                    entrada.nextLine();
+
+                    Medico medicoEscolhido = medicosDisponiveis.get(escolha - 1);
+
+                    Paciente paciente = new Paciente(nome, idade, CPF);
+                    Consulta consulta = new Consulta(data, horario, medicoEscolhido.getNomeMedico());
+
+                    //Fazendo o agendamento da consulta
+                    consultasAgendadas.agendarConsulta(paciente, consulta);
+                    break;
+
+                case  2:
+
+                    break;
+
+            }
+        }
+
+
+    }
 }
